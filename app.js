802e1b07d40a7daf14b7abb71f679d99bd7f28c6
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-06-08 16:58:01 -0400

    building game logic and components

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('it works')
+let lastRenderTime = 0
+
+
+function main(currentTime) {
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+     if (secondsSinceLastRender < 1 / snakeSpeed) return
+
+
+
+     console.log('render')
+    lastRenderTime = currentTime
+
+update()
+draw()
+
+
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+
+}
+
+function draw() {
+
+}
